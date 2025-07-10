658b365603126f0727ff2dfd0ee0023836d40283
Hadi07009 <hadi.mbstu@gmail.com>
2025-07-10 17:04:35 +0600

    Creating a Web Forms Application

---
+++ ERP/HR/UI/EmployeeInformation.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HR.UI
+{
+    public partial class EmployeeInformation : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnShow_Click(object sender, EventArgs e)
+        {
+            lblShowEmployeeName.Text = string.Empty;
+            lblShowEmployeeName.Text = txtEmployeeName.Text;
+        }
+    }
+}
