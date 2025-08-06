d5efed09682f162f756f9744d9cbd9e6cd1f9f7e
Hadi07009 <hadi.mbstu@gmail.com>
2025-08-06 14:16:25 +0600

    Class 08- Entity Framework

--- ERP/HR/Global.asax
+++ ERP/HR/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HR.Global" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HR.MvcApplication" Language="C#" %>
---
+++ ERP/HR/Models/EmployeeInformation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HR.Models
+{
+    public class EmployeeInformation
+    {
+        string _name;
+        int _id;
+        string _email;
+        string _mobile;
+        string _address;
+    }
+}
